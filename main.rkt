79463debc35dbe612dead18d857d16645c8d55cd
Yuhao Dong <yd2dong@csclub.uwaterloo.ca>
2015-06-04 23:00:27 -0400

    fixed contracts

--- main.rkt
+++ main.rkt
@@ -7,9 +7,13 @@
                                                   (Values Input-Port Output-Port))])
 
 (require/typed/provide "llobfs.rkt"
-                       [llobfs-handshake/server (-> Bytes Input-Port Output-Port)]
-                       [llobfs-handshake/client (-> Bytes Input-Port Output-Port)])
+                       [llobfs-handshake/server (-> Bytes Input-Port Output-Port
+                                                    (Values Input-Port Output-Port))]
+                       [llobfs-handshake/client (-> Bytes Input-Port Output-Port
+                                                    (Values Input-Port Output-Port))])
 
 (require/typed/provide "hlobfs.rkt"
-                       [hlobfs-handshake/server (-> Bytes Input-Port Output-Port)]
-                       [hlobfs-handshake/client (-> Bytes Input-Port Output-Port)])
+                       [hlobfs-handshake/server (-> Bytes Input-Port Output-Port
+                                                    (Values Input-Port Output-Port))]
+                       [hlobfs-handshake/client (-> Bytes Input-Port Output-Port
+                                                    (Values Input-Port Output-Port))])
